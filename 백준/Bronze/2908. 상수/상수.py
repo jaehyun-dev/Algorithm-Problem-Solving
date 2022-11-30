5ce542cdc63a4729efb7800cc32fd6185735ae47
jaehyun-dev <71001479+jaehyun-dev@users.noreply.github.com>
2022-12-01 00:50:12 +0900

    Update 상수.py

--- 백준/Bronze/2908. 상수/상수.py
+++ 백준/Bronze/2908. 상수/상수.py
@@ -5,3 +5,11 @@ for i in range(3):
     d.append(b[2 - i])
 e, f = int(c[0] + c[1] + c[2]), int(d[0] + d[1] + d[2])
 print(max(e, f))
+
+# habadjen의 답
+n1, n2 = input().split()
+n1, n2 = int(n1[::-1]), int(n2[::-1])
+if n1 > n2:
+    print(n1)
+else:
+    print(n2)
