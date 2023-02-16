d1acdc93e49aebdb5426d3e88a38809a91edbbab
jaehyun-dev <71001479+jaehyun-dev@users.noreply.github.com>
2023-02-16 19:55:08 +0900

    [Bronze IV] Title: 단순한 문제 (Small), Time: 3344 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/25494. 단순한 문제 （Small）/단순한 문제 （Small）.py
@@ -0,0 +1,10 @@
+T = int(input())
+for _ in range(T):
+    a, b, c = map(int, input().split())
+    count = 0
+    for x in [*range(1, a + 1)]:
+        for y in [*range(1, b + 1)]:
+            for z in [*range(1, c + 1)]:
+                if x % y == y % z == z % x:
+                    count += 1
+    print(count)
