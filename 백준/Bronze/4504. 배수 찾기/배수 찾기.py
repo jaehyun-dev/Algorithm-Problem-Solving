99fa596000ba09b92391f4c7b3f8d5400eb8bef3
jaehyun-dev <71001479+jaehyun-dev@users.noreply.github.com>
2023-05-04 23:19:50 +0900

    [Bronze III] Title: 배수 찾기, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/4504. 배수 찾기/배수 찾기.py
@@ -0,0 +1,9 @@
+n = int(input())
+while 1:
+    a = int(input())
+    if a == 0:
+        break
+    print(f"{a} is ", end="")
+    if a % n != 0:
+        print("NOT ", end="")
+    print(f"a multiple of {n}.")
