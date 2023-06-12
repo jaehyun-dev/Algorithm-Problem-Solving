4b265193f54e2175c7eaf17cf0c04a98a8885d5d
jaehyun-dev <71001479+jaehyun-dev@users.noreply.github.com>
2023-06-12 12:48:12 +0900

    [Bronze V] Title: Helicopter, Time: 56 ms, Memory: 5316 KB -BaekjoonHub

---
+++ 백준/Bronze/14337. Helicopter/Helicopter.vb
@@ -0,0 +1,13 @@
+Imports System
+Module Program
+    Sub Main(args As String())
+        Dim text As String = "      ===================" & Environment.NewLine &
+                             "          ____||___" & Environment.NewLine &
+                             "\ /      /       []\" & Environment.NewLine &
+                             " X=======           \__" & Environment.NewLine &
+                             "/ \      \____________|" & Environment.NewLine &
+                             "            ||  ||" & Environment.NewLine &
+                             "         \-----------/"
+        Console.WriteLine(text)
+    End Sub
+End Module
