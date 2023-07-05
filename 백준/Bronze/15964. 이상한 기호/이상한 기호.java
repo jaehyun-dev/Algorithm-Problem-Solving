e5a74559b4512b108dff8b3a361c574f7619839f
jaehyun-dev <71001479+jaehyun-dev@users.noreply.github.com>
2023-07-06 08:01:44 +0900

    [Bronze V] Title: 이상한 기호, Time: 204 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A, B;
+		A = sc.nextInt();
+		B = sc.nextInt();
+		int result = newOperation(A, B);
+		System.out.println(result);
+	}
+	
+	public static int newOperation(int A, int B) {
+		int first = A + B;
+		int second = A - B;
+		int res = first * second;
+		return res;
+	}	
+}
