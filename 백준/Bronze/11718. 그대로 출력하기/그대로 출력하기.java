79770459ffed1337fed2b2ced9c1d518b8f4323c
jaehyun-dev <71001479+jaehyun-dev@users.noreply.github.com>
2023-07-06 07:47:30 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 232 ms, Memory: 17988 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		while(sc.hasNext()) {
+			System.out.println(sc.nextLine());
+		}
+	}
+}
