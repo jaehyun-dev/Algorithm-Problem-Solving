31f5338934944d596f2122533e5c33fccdb7b02b
jaehyun-dev <71001479+jaehyun-dev@users.noreply.github.com>
2023-10-09 17:33:04 +0900

    [Silver I] Title: RGB거리, Time: 132 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -1,17 +1,8 @@
 import sys
 input = sys.stdin.readline
 N = int(input())
-dp = [0, 0, 0]
-a = list(map(int, input().split()))
-b = list(map(int, input().split()))
-dp[0] = min(a[1], a[2]) + b[0]
-dp[1] = min(a[0], a[2]) + b[1]
-dp[2] = min(a[0], a[1]) + b[2]
-for _ in range(N - 2):
-    c = list(map(int, input().split()))
-    temp = list(dp)
-    temp[0] = min(dp[1], dp[2]) + c[0]
-    temp[1] = min(dp[0], dp[2]) + c[1]
-    temp[2] = min(dp[0], dp[1]) + c[2]
-    dp = list(temp)
+dp = list(map(int, input().split()))
+for _ in range(N - 1):
+    a = list(map(int, input().split()))
+    dp = [min(dp[1], dp[2]) + a[0], min(dp[0], dp[2]) + a[1], min(dp[0], dp[1]) + a[2]]
 print(min(dp))
