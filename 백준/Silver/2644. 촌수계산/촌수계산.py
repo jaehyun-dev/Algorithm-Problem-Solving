920faa1b27a48fb0680885ef18ff0e1ea3459dfa
jaehyun-dev <71001479+jaehyun-dev@users.noreply.github.com>
2023-10-12 11:01:53 +0900

    [Silver II] Title: 촌수계산, Time: 68 ms, Memory: 34044 KB -BaekjoonHub

---
+++ 백준/Silver/2644. 촌수계산/촌수계산.py
@@ -0,0 +1,26 @@
+import sys, collections
+input = sys.stdin.readline
+n = int(input())
+
+graph = [[0] * (n + 1) for _ in range(n + 1)]
+vis = [0] * (n + 1)
+
+a, b = map(int, input().split())
+m = int(input())
+for _ in range(m):
+    x, y = map(int, input().split())
+    graph[x][y] = graph[y][x] = 1
+
+q = collections.deque()
+q.append(a)
+vis[a] = 1
+c = 1
+while q:
+    x = q.popleft()
+    for i in range(1, n + 1):
+        if graph[x][i] == 1 and not vis[i]:
+            graph[a][i] = graph[a][x] + 1
+            vis[i] = 1
+            q.append(i)
+ans = graph[a][b]
+print(ans if ans else -1)
