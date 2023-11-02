17bf4d33996db164923558070b3771d1d62cd7df
jaehyun-dev <71001479+jaehyun-dev@users.noreply.github.com>
2023-11-02 09:16:41 +0900

    [Silver II] Title: 상자넣기, Time: 224 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1965. 상자넣기/상자넣기.py
@@ -0,0 +1,8 @@
+n = int(input())
+arr = list(map(int, input().split()))
+dp = [1] * n
+for i in range(1, n):
+    for j in range(i):
+        if arr[j] < arr[i]:
+            dp[i] = max(dp[j] + 1, dp[i])
+print(max(dp))
