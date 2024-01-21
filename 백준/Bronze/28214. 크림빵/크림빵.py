9662433ec21fb6a6865b597d891fe1653aa83678
jaehyun-dev <kjh105702@naver.com>
2024-01-21 23:03:33 +0900

    [Bronze III] Title: 크림빵, Time: 56 ms, Memory: 31252 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/28214. 크림빵/크림빵.py
+++ 백준/Bronze/28214. 크림빵/크림빵.py
@@ -1,8 +1,8 @@
 N, K, P = map(int, input().split())
 b = list(map(int, input().split()))
 ans = 0
-cnt = 0
 for n in range(N):
+    cnt = 0
     for k in range(K):
         if b[n * K + k] == 0:
             cnt += 1
