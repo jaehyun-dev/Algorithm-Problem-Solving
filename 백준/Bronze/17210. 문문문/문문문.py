0ad2f289d32194337dc211626dbb8836a8b3063d
jaehyun-dev <kjh105702@naver.com>
2024-02-26 19:52:52 +0900

    [Bronze III] Title: 문문문, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/17210. 문문문/문문문.py
@@ -0,0 +1,12 @@
+N = int(input())
+if 5 < N:
+    print("Love is open door")
+else:
+    a = int(input())
+    flag = True
+    for _ in range(N - 1):
+        if a:
+            print(0)
+        else:
+            print(1)
+        a = not a
