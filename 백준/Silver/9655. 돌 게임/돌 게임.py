2b42c00e134bc3b211c1a187ade5be6cf584c60c
jaehyun-dev <kjh105702@naver.com>
2024-04-08 23:31:56 +0900

    [Silver V] Title: 돌 게임, Time: 40 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.py
+++ 백준/Silver/9655. 돌 게임/돌 게임.py
@@ -1,8 +1,5 @@
 N = int(input())
-a = [False] * max(4, N + 1)
-a[1] = True
-a[3] = True
-for i in range(4, N + 1):
-    if a[i - 4]:
-        a[i] = True
-print("SK" if a[N] else "CY")
+if N % 4 in (1, 3):
+    print("SK")
+else:
+    print("CY")
