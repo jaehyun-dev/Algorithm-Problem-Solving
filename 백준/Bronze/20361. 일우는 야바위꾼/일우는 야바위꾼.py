5a298508234f2c0bb180bb2066577c3734a152f0
jaehyun-dev <kjh105702@naver.com>
2024-05-11 23:00:42 +0900

    [Bronze III] Title: 일우는 야바위꾼, Time: 144 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/20361. 일우는 야바위꾼/일우는 야바위꾼.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+N, X, K = map(int, input().split())
+ans = X
+for _ in range(K):
+    A, B = map(int, input().split())
+    if A == ans:
+        ans = B
+    elif B == ans:
+        ans = A
+print(ans)
