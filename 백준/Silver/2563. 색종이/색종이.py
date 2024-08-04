3d8d9f03bc7b1dac7666abcbed45adea504ebf15
jaehyun-dev <kjh105702@naver.com>
2024-08-04 14:18:44 +0900

    [Silver V] Title: 색종이, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -0,0 +1,8 @@
+graph = [([0] * 100) for _ in range(100)]
+N = int(input())
+for _ in range(N):
+    x, y = map(int, input().split())
+    for i in range(x, x + 10):
+        for j in range(y, y + 10):
+            graph[i][j] = 1
+print(sum(list(map(lambda x:sum(x), graph))))
