0e6dbee56480078dfc95d1f2e573405c3638c5d4
jaehyun-dev <kjh105702@naver.com>
2024-10-19 23:00:47 +0900

    [Silver I] Title: 내려가기 2, Time: 304 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15645. 내려가기 2/내려가기 2.py
+++ 백준/Silver/15645. 내려가기 2/내려가기 2.py
@@ -1,16 +1,9 @@
 import sys
 input = sys.stdin.readline
 N = int(input())
-graph = []
-for _ in range(N):
-    graph.append(list(map(int, input().split())))
-dp = [[(graph[0]) for _ in range(2)]]
-for i in range(N - 1):
-    dp.append([([0] * 3) for _ in range(2)])
-    dp[i + 1][0][0] = max(dp[i][0][:2]) + graph[i + 1][0]
-    dp[i + 1][0][1] = max(dp[i][0]) + graph[i + 1][1]
-    dp[i + 1][0][2] = max(dp[i][0][1:]) + graph[i + 1][2]
-    dp[i + 1][1][0] = min(dp[i][1][:2]) + graph[i + 1][0]
-    dp[i + 1][1][1] = min(dp[i][1]) + graph[i + 1][1]
-    dp[i + 1][1][2] = min(dp[i][1][1:]) + graph[i + 1][2]
-print(max(dp[-1][0]), min(dp[-1][1]))
+l = list(map(int, input().split()))
+dp = [list(l), list(l)]
+for _ in range(N - 1):
+    t = list(map(int, input().split()))
+    dp[0][0], dp[0][1], dp[0][2], dp[1][0], dp[1][1], dp[1][2] = max(dp[0][:2]) + t[0], max(dp[0]) + t[1], max(dp[0][1:]) + t[2], min(dp[1][:2]) + t[0], min(dp[1]) + t[1], min(dp[1][1:]) + t[2]
+print(max(dp[0]), min(dp[1]))
